347de8d6abedaaa9e468eee37b79cd4613885e4d
san <sanwaitun@gmail.com>
2016-07-30 03:06:39 -0700

    add assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Please only use an inversible square matrix that can be solved by Solve()
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a matrix and set inverse matrix
+##
+## How to test:
+## 1. Create a matrix
+##      x <- matrix (1:4,nrow = 2,ncol = 2)
+##      matx <- makeCacheMatrix(x)
+## 2. get the matrix using get()
+##      matx$get()
+## 3. Set the inverse of the matrix using setinmatrix()
+##      matx$setinmatrix(solve(matx$get()))
+## 4. get the inverse matrix using getinmatrix()
+##      matx$getinmatrix()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinmatrix <- function(inmatrix) m <<- inmatrix
+        getinmatrix <- function() m
+        list(set = set, get = get,
+             setinmatrix = setinmatrix,
+             getinmatrix = getinmatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function check if the inverser function has been calculated using makeCachematrix()
+## How to Test:
+##      cacheSolve(mmat)
+## If the inverse matrix has been calculated, this function will output the calculated inverse matrix 
+## and the message "getting cached data". If not, this function will calcultate and output the inverse matrix
+## 
 
 cacheSolve <- function(x, ...) {
+
+        m <- x$getinmatrix()
+        
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setinmatrix(m)
+        m
         ## Return a matrix that is the inverse of 'x'
+        
 }
